2bf61f4db6f1dc46899fae6b167d4ad7ef149881
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2022-08-08 15:35:37 +0100

    first commit

---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="notes">
+
+      <div class="card has-background-success-dark p-4 mb-5">
+        <div class="field">
+          <div class="control">
+            <textarea 
+                v-model="newNote"
+                class="textarea" 
+                placeholder="Add a new note" 
+            />
+          </div>
+        </div>
+
+        <div class="field is-grouped is-grouped-right">
+          <div class="control">
+            <button class="button is-link
+              has-background-success"
+            >
+              Add New Notes
+            </button>
+          </div>
+          <div class="control">
+            <button class="button is-link is-light">Cancel</button>
+          </div>
+        </div>
+      </div>
+
+      <div 
+        v-for="note in notes"
+        :key="note.id"
+        class="card mb-4"
+      >
+          <div class="card-content">
+              <div class="content">
+                {{ note.content }}
+              </div>
+          </div>
+          <footer class="card-footer">
+            <a href="#" class="card-footer-item">Edit</a>
+            <a href="#" class="card-footer-item">Delete</a>
+          </footer>
+      </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+/**
+ *  imports 
+ */
+
+  import { ref } from 'vue'
+
+/**
+ * notes
+ */
+
+  const newNote = ref('Telly tuby second guess')
+
+  const notes = ref ([
+      {
+        id: "id1",
+        content: "lorem ipsum dolor sit amet, consectetur adipiscing"
+      },
+      {
+        id: "id2",
+        content: "This is a shorter note! woo!",
+      },
+  ])
+
+  
+</script>
+
+
+<style>
+/* @media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+} */
+</style>
